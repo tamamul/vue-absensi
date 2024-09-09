cd1ce359f7610eb5fe9c9f59d6961253d187f9e7
mahatmaArrayyan <muh.mahatma@gmail.com>
2024-09-09 11:17:28 +0800

    styling card

--- src/views/Kanban.vue
+++ src/views/Kanban.vue
@@ -1,11 +1,18 @@
 <template>
+    <!-- Title -->
     <div class="col-span-12 flex items-center gap-14">
         <h2 class="font-medium text-3xl text-def">Daftar Tugas Bulan Ini</h2>
-        <Button icon="pi pi-plus" label="Tambah Tugas" @click="visible = true" />
-        <Button icon="pi pi-upload" label="Update" @click="update()" />
+        <div class="flex gap-3">
+            <Button icon="pi pi-plus" label="Tambah Tugas" @click="visible = true" />
+            <Button icon="pi pi-upload" label="Update" @click="update()" />
+        </div>
     </div>
+
+    <!-- Kanban cards -->
     <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
-        <template #title>To do's</template>    
+        <template #title>To do's</template>
+
+        <!-- // TODO Card To do's  -->
         <template #content>
             <draggable 
                 v-model="kanban.list1" 
@@ -16,9 +23,15 @@
                 class="flex flex-col gap-5"
             >
                 <template #item="{element}">
-                    <Card class="shadow-md">
+                    <Card class="shadow-md border-l-yellow-500 border-l-8">
                         <template #title>
-                            {{element.name}}
+                            <div class="grid grid-cols-12 gap-5">
+                                <div class="col-span-10">{{element.name}}</div>
+                                <div class="col-span-2">
+                                    <Button type="button" icon="pi pi-ellipsis-h" @click="toggle" aria-haspopup="true" aria-controls="overlay_tmenu" text severity="secondary" />
+                                    <TieredMenu ref="menu" id="overlay_tmenu" :model="items" popup />
+                                </div>
+                            </div>
                         </template>
                         <template #content>
                             {{element.deskripsi}}
@@ -28,6 +41,8 @@
             </draggable>
         </template>
     </Card>
+
+    <!-- // ? Card In Progress  -->
     <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
         <template #title>In Progress</template>
         <template #content>
@@ -40,55 +55,7 @@
                 class="flex flex-col gap-5"
             >
                 <template #item="{element}">
-                    <Card class="shadow-md">
-                        <template #title>
-                            {{element.name}}
-                        </template>
-                        <template #content>
-                            {{element.deskripsi}}
-                        </template>
-                    </Card>
-                </template>
-            </draggable>
-        </template>
-    </Card>
-    <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
-        <template #title>Trial Error</template>    
-        <template #content>         
-            <draggable 
-                v-model="kanban.list3" 
-                group="tasks" 
-                @start="drag=true" 
-                @end="drag=false" 
-                item-key="id"
-                class="flex flex-col gap-5"
-            >
-                <template #item="{element}">
-                    <Card class="shadow-md">
-                        <template #title>
-                            {{element.name}}
-                        </template>
-                        <template #content>
-                            {{element.deskripsi}}
-                        </template>
-                    </Card>
-                </template>
-            </draggable>
-        </template>
-    </Card>
-    <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
-        <template #title>Completed</template>    
-        <template #content>         
-            <draggable 
-                v-model="kanban.list4" 
-                group="tasks" 
-                @start="drag=true" 
-                @end="drag=false" 
-                item-key="id"
-                class="flex flex-col gap-5"
-            >
-                <template #item="{element}">
-                    <Card class="shadow-md">
+                    <Card class="shadow-md border-l-blue-500 border-l-8">
                         <template #title>
                             {{element.name}}
                         </template>
@@ -101,6 +68,59 @@
         </template>
     </Card>
 
+    <!-- // ! Card Trial error  -->
+    <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
+        <template #title>Trial Error</template>    
+        <template #content>         
+            <draggable 
+                v-model="kanban.list3" 
+                group="tasks" 
+                @start="drag=true" 
+                @end="drag=false" 
+                item-key="id"
+                class="flex flex-col gap-5"
+            >
+                <template #item="{element}">
+                    <Card class="shadow-md border-l-red-700 border-l-8">
+                        <template #title>
+                            {{element.name}}
+                        </template>
+                        <template #content>
+                            {{element.deskripsi}}
+                        </template>
+                    </Card>
+                </template>
+            </draggable>
+        </template>
+    </Card>
+
+    <!-- // * Card Coompleted  -->
+    <Card class="shadow-md col-span-12 lg:col-span-6 xl:col-span-3">
+        <template #title>Completed</template>    
+        <template #content>         
+            <draggable 
+                v-model="kanban.list4" 
+                group="tasks" 
+                @start="drag=true" 
+                @end="drag=false" 
+                item-key="id"
+                class="flex flex-col gap-5"
+            >
+                <template #item="{element}">
+                    <Card class="shadow-md border-l-lime-300 border-l-8">
+                        <template #title>
+                            {{element.name}}
+                        </template>
+                        <template #content>
+                            {{element.deskripsi}}
+                        </template>
+                    </Card>
+                </template>
+            </draggable>
+        </template>
+    </Card>
+
+    <!-- // ? Modal untuk upload task baru -->
     <Dialog v-model:visible="visible" modal header="Tambahkan Tugas" :style="{ width: '32rem' }">
         <span class="text-surface-500 dark:text-surface-400 block mb-8">Tambahkan list tugas untuk bulan ini.</span>
         <div class="flex items-center gap-4 mb-4">
@@ -116,6 +136,9 @@
             <Button type="button" label="Save" @click="add()"></Button>
         </div>
     </Dialog>
+
+    <!-- // ? Roti -->
+    <Toast />
 </template>
 
 <script>
@@ -139,43 +162,21 @@ export default {
                 name: '',
                 deskripsi: ''
             },
+
+            // ? menu popup untuk edit or delete kanban
             items: [
                 {
-                    label: 'Add',
-                    icon: 'pi pi-pencil',
-                    command: () => {
-                        this.$toast.add({ severity: 'info', summary: 'Add', detail: 'Data Added', life: 3000 });
-                    }
+                    label: 'Edit',
+                    icon: 'pi pi-pencil'
                 },
                 {
-                    label: 'Update',
-                    icon: 'pi pi-refresh',
-                    command: () => {
-                        this.$toast.add({ severity: 'success', summary: 'Update', detail: 'Data Updated', life: 3000 });
-                    }
+                    separator: true
                 },
                 {
                     label: 'Delete',
-                    icon: 'pi pi-trash',
-                    command: () => {
-                        this.$toast.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted', life: 3000 });
-                    }
-                },
-                {
-                    label: 'Upload',
-                    icon: 'pi pi-upload',
-                    command: () => {
-                        this.$router.push('/fileupload');
-                    }
-                },
-                {
-                    label: 'Vue Website',
-                    icon: 'pi pi-external-link',
-                    command: () => {
-                        window.location.href = 'https://vuejs.org/';
-                    }
+                    icon: 'pi pi-trash'
                 }
-            ]
+            ],
         }
     },
     methods: {
@@ -188,9 +189,8 @@ export default {
             }
             gettingData();
         },
-        toggle(event) {
-            this.$refs.menu.toggle(event);
-        },
+
+        // ? Push To Object List 1
         add() {
             this.kanban.list1.push({ 
                 id: Date.now(),
@@ -200,7 +200,10 @@ export default {
             this.visible = false;
             this.formKanban.name = '';
             this.formKanban.deskripsi = '';
+            this.$toast.add({ severity: 'success', summary: `Menambahkan tugas baru`, detail: 'Tugas telah ditambah', life: 3000 });
         },
+
+        // ? Update all objects kanban
         async update() {
             try {
                 const response = await fetch("http://localhost:3000/kanban", {
@@ -219,9 +222,13 @@ export default {
                 console.error('Error:', error);
                 this.$toast.add({ severity: 'error', summary: 'Update Failed', detail: 'Data Update Failed', life: 3000 });
             }
+        },
+        toggle(event) {
+            this.$refs.menu.toggle(event);
         }
     },
     mounted() {
+        // ? Run getKanban run load
         this.getKanban()
     },
 }
