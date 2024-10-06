f42ea0194d59572ea395829f807c7f3e43cba316
mahatmaArrayyan <muh.mahatma@gmail.com>
2024-10-06 22:41:13 +0800

    machine learning

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
         return {
             default: {
                 title: 'ABSENSI QR CODE',
-                image: 'http://127.0.0.1:8000/storage/',
+                image: 'http://127.0.0.1:8000/storage/', 
                 token: localStorage.getItem('token')
             }
         };
