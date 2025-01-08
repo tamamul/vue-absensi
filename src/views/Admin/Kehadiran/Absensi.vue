9f0b34a27308aa636f4388802a00dd09ccd4893c
mahatmaArrayyan <muh.mahatma@gmail.com>
2025-01-08 09:10:12 +0800

    hapus data

--- src/views/Admin/Kehadiran/Absensi.vue
+++ src/views/Admin/Kehadiran/Absensi.vue
@@ -113,9 +113,6 @@
                 </p>
                 <p v-show="pesan" class="col-span-12 font-semibold text-red-500">
                     Isi Semua Input Data!
-                    {{ jam_keluar }}
-                    {{ jam_masuk }}
-                    {{ status }}
                 </p>
 
                 <div class="col-span-12 w-full gap-1">
